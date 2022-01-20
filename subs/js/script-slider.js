8a513a6057d7631d2ab3dc3a0c07da4a1d6a2334
jinach0i <ginach0irk@gmail.com>
2022-01-20 19:59:39 +0900

    slider.html is completed

--- subs/js/script-slider.js
+++ subs/js/script-slider.js
@@ -1,15 +1,33 @@
-var slide=$('#slider ul li'),
-current = 0;
-setInterval(()=>{
-var prev = slide.eq(current);
-current++;
-move(prev, 1, 0);
-if(current==slide.size()){
-current=0;
-};
-var next = slide.eq(current);
-move(next, 0, 1);
-},3000);
-function move(tg, start, end){
-tg.css({opacity: start}).stop().animate({opacity:end});
-};
+$(function () {
+    var slide1=$('#slide1 ul li'),
+    current1 = 0;
+    setInterval(()=>{
+    var prev1 = slide1.eq(current1);
+    current1++;
+    move1(prev1, 1, 0);
+    if(current1==slide1.size()){
+    current1=0;
+    };
+    var next1 = slide1.eq(current1);
+    move1(next1, 0, 1);
+    },3000);
+    function move1(tg1, start, end){
+    tg1.css({opacity: start}).stop().animate({opacity:end});
+    };
+
+    var slide2 = $('#slide2 ul li'),
+    current2 = 0;
+    setInterval(() => {
+    var prev2 = slide2.eq(current2);
+    current2++;
+    move2(prev2,0,'100%');
+    if (current2==slide2.size()) {
+    current2=0;
+    }
+    var next2 = slide2.eq(current2);
+    move2(next2,'-100%',0);
+    }, 2000);
+    function move2(tg1, start, end) {
+    tg1.css({left:start}).stop().animate({left:end},1000)
+    };
+})
